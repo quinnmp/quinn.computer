56827a0c3a1c5dff7370d9445a780e45d4fb4c9f
Quinn Pfeifer <72775766+QuinnP2910@users.noreply.github.com>
2024-03-23 20:06:56 -0700

    Move html content to different files

--- app.js
+++ app.js
@@ -1,4 +1,29 @@
 $(document).ready(function () {
+    let pages = [
+        "home",
+        "academics",
+        "robotics",
+        "volunteering",
+        "job",
+        "skills",
+        "awards",
+        "projects",
+        "hobbies",
+        "résumé",
+        "contact",
+    ];
+    function loadTextIntoDiv(filePath, divId) {
+        $.get(filePath, function (data) {
+            $(divId).html(data);
+        }).fail(function () {
+            console.error("There was a problem fetching the file:", filePath);
+        });
+    }
+
+    pages.forEach((page) => {
+        loadTextIntoDiv(`pages/${page}.html`, `#v-pills-${page}`);
+    });
+
     $(".submit-btn").click(function () {
         const firstName = $("#firstName").val();
         const lastName = $("#lastName").val();
@@ -12,6 +37,7 @@ $(document).ready(function () {
         console.log(mailtoLink);
         window.location.href = mailtoLink;
     });
+
     $(".nav-link").click(function () {
         console.log("NAVLINK");
         $("#main-img").attr("src", "images/" + $(this).attr("id") + ".jpg");
